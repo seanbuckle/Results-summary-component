ccb27f1d7d45b4dfb58b633199bdd6d46252b23c
seanbuckle <seanlbuckle@gmail.com>
2024-04-01 23:29:29 +0100

    Refactor code to use async/await and improve readability

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
-window.addEventListener('load', function() {
-    url = "data.json";
-    fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        const rImg = data[0].icon;
-        const mImg = data[1].icon;
-        const vImg = data[2].icon;
-        const viImg = data[3].icon;
-        const rTxt = data[0].category;
-        const mTxt = data[1].category;
-        const vTxt = data[2].category;
-        const viTxt = data[3].category;
-        const rScore = data[0].score;
-        const mScore = data[1].score;
-        const vScore = data[2].score;
-        const viScore = data[3].score;
-        document.getElementById("rImg").src = rImg;
-        document.getElementById("mImg").src = mImg;
-        document.getElementById("vImg").src = vImg;
-        document.getElementById("viImg").src = viImg;
-        document.getElementById("rTxt").innerHTML = rTxt;
-        document.getElementById("mTxt").innerHTML = mTxt;
-        document.getElementById("vTxt").innerHTML = vTxt;
-        document.getElementById("viTxt").innerHTML = viTxt;
-        document.getElementById("rScore").innerHTML = rScore;
-        document.getElementById("mScore").innerHTML = mScore;
-        document.getElementById("vScore").innerHTML = vScore;
-        document.getElementById("viScore").innerHTML = viScore;
-    })
+"use strict";
+window.addEventListener("load", async () => {
+    try {
+        const response = await fetch("data.json");
+        const data = await response.json();
+        const ids = ["r", "m", "v", "vi"];
+        for (let i = 0; i < data.length; i++) {
+            const { icon, category, score } = data[i];
+            const imgId = `${ids[i]}Img`;
+            const txtId = `${ids[i]}Txt`;
+            const scoreId = `${ids[i]}Score`;
+            const imgElement = document.getElementById(imgId);
+            imgElement.src = icon;
+            document.getElementById(txtId).textContent = category;
+            document.getElementById(scoreId).textContent = score.toString();
+        }
+    }
+    catch (error) {
+        console.error("Error fetching data:", error);
+    }
 });
